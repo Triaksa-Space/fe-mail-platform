fe9fd48d54f98b409512ac1070d3a6a6e2c87ebd
Bamas Angkasa <inilobamas@gmail.com>
2024-12-07 20:50:04 +0700

    send login

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ export default function LandingPage() {
   const [failedAttempts, setFailedAttempts] = useState(0);
   const [lockoutTime, setLockoutTime] = useState<number | null>(null);
   const [countdown, setCountdown] = useState<number>(0);
+  const [loginEmail, setLoginEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
 
@@ -61,19 +62,20 @@ export default function LandingPage() {
 
     if (lockoutTime) return;
 
-    const formData = new FormData(e.currentTarget);
-    const email = formData.get("email")?.toString();
+    // const formData = new FormData(e.currentTarget);
+    // const email = formData.get("email")?.toString();
     // const password = formData.get("password")?.toString();
+    console.log("loginEmail", loginEmail)
     console.log("password", password)
 
-    if (!email || !password) return;
+    if (!loginEmail || !password) return;
 
     setIsLoading(true);
 
     try {
       const response = await axios.post(
         `${process.env.NEXT_PUBLIC_API_BASE_URL}/login`,
-        { email, password },
+        { email: loginEmail, password: password },
         {
           headers: {
             "Content-Type": "application/json",
@@ -143,101 +145,97 @@ export default function LandingPage() {
 
   return (
     <>
-    <div className="flex flex-col min-h-screen justify-between bg-white p-4 pt-8">
-      <div className="w-full max-w-sm mx-auto space-y-8">
-        <div className="space-y-2 text-center">
-          <h1 className="text-xl font-semibold tracking-tight">
-            Where Simplicity Meets Speed.
-          </h1>
+      <div className="flex flex-col min-h-screen justify-between bg-white p-4 pt-8">
+        <div className="w-full max-w-sm mx-auto space-y-8">
+          <div className="space-y-2 text-center">
+            <h1 className="text-xl font-semibold tracking-tight">
+              Where Simplicity Meets Speed.
+            </h1>
+          </div>
+          <form onSubmit={onSubmit} className="space-y-6 p-4">
+            <div className="space-y-2">
+              <Label htmlFor="email" className="text-base">
+                Email
+              </Label>
+              <div className="relative">
+                <Mail className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
+                <Input
+                  id="email"
+                  name="email"
+                  placeholder="example@mailria.com"
+                  required
+                  value={loginEmail}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    setLoginEmail(value.replace(/\s/g, '')); // Remove spaces
+                  }}
+                  type="text"
+                  className="pl-10 h-12 text-base border-gray-200"
+                />
+              </div>
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password" className="text-base">
+                Password
+              </Label>
+              <div className="relative">
+                <Lock className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
+                <PasswordInput
+                  id="password"
+                  placeholder="Input Password"
+                  value={password}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    setPassword(value.replace(/\s/g, '')); // Remove spaces
+                  }}
+                  showPassword={showPassword}
+                  setShowPassword={setShowPassword}
+                />
+              </div>
+            </div>
+            <Button
+              className={`w-full h-12 text-base font-bold ${lockoutTime || !loginEmail || !password
+                  ? "bg-gray-400 cursor-not-allowed text-black"
+                  : "bg-[#F7D65D] hover:bg-[#F7D65D]/90 text-black"
+                }`}
+              type="submit"
+              disabled={isLoading || !!lockoutTime || !loginEmail || !password}
+            >
+              {lockoutTime
+                ? `Login (${countdown})`
+                : isLoading
+                  ? "Signing in..."
+                  : "Login"}
+            </Button>
+            {failedAttempts === 3 && (
+              <p className="text-xs text-red-600 text-center">
+                Careful! One more failed attempt will disable login for 10 minutes.
+              </p>
+            )}
+            {lockoutTime ? (
+              <p className="text-xs text-red-600 text-left">
+                Too many failed attempts. Try again in {Math.ceil(countdown / 60)} minutes.
+              </p>
+            ) : null}
+          </form>
         </div>
-        <form onSubmit={onSubmit} className="space-y-6 p-4">
-          <div className="space-y-2">
-            <Label htmlFor="email" className="text-base">
-              Email
-            </Label>
-            <div className="relative">
-              <Mail className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
-              <Input
-                id="email"
-                name="email"
-                placeholder="example@mailria.com"
-                required
-                type="text"
-                className="pl-10 h-12 text-base border-gray-200"
-              />
-            </div>
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="password" className="text-base">
-              Password
-            </Label>
-            <div className="relative">
-              <Lock className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
-              {/* <Input
-                id="password"
-                name="password"
-                placeholder="Input password"
-                required
-                type="password"
-                className="pl-10 h-12 text-base border-gray-200"
-              /> */}
-              <PasswordInput
-              id="password"
-              placeholder="Input Password"
-              value={password}
-              onChange={(e) => {
-                const value = e.target.value;
-                setPassword(value.replace(/\s/g, '')); // Remove spaces
-              }}
-              showPassword={showPassword}
-              setShowPassword={setShowPassword}
-            />
-            </div>
-          </div>
-          <Button
-            className={`w-full h-12 text-base font-medium ${
-              lockoutTime
-                ? "bg-gray-400"
-                : "bg-[#F7D65D] hover:bg-[#F7D65D]/90 text-black"
-            }`}
-            type="submit"
-            disabled={isLoading || !!lockoutTime}
-          >
-            {lockoutTime
-              ? `Login (${countdown})`
-              : isLoading
-              ? "Signing in..."
-              : "Login"}
-          </Button>
-          {failedAttempts === 3 && (
-            <p className="text-xs text-red-600 text-center">
-              Careful! One more failed attempt will disable login for 10 minutes.
-            </p>
-          )}
-          {lockoutTime ? (
-            <p className="text-xs text-red-600 text-left">
-              Too many failed attempts. Try again in {Math.ceil(countdown / 60)} minutes.
-            </p>
-          ) : null}
-        </form>
+        <div className="w-full max-w-sm mx-auto mb-2 space-y-4 p-4 text-left">
+          <h2 className="text-l font-semibold">
+            Looking for reliable email services?
+          </h2>
+          <p className="text-sm text-gray-600">
+            Mailria has you covered! Drop us a message at{" "}
+            <a
+              href="mailto:support@mailria.com"
+              className="text-blue-600 hover:underline"
+            >
+              support@mailria.com
+            </a>{" "}
+            for more details.
+          </p>
+        </div>
       </div>
-      <div className="w-full max-w-sm mx-auto mb-2 space-y-4 p-4 text-left">
-        <h2 className="text-l font-semibold">
-          Looking for reliable email services?
-        </h2>
-        <p className="text-sm text-gray-600">
-          Mailria has you covered! Drop us a message at{" "}
-          <a
-            href="mailto:support@mailria.com"
-            className="text-blue-600 hover:underline"
-          >
-            support@mailria.com
-          </a>{" "}
-          for more details.
-        </p>
-      </div>
-    </div>
-    <Toaster />
+      <Toaster />
     </>
   );
 }
