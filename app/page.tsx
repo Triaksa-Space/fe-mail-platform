b73cdbf63c8e72ad97eef405297350b289d51c94
Bamas Angkasa <inilobamas@gmail.com>
2024-12-08 21:13:22 +0700

    input pass

--- app/page.tsx
+++ app/page.tsx
@@ -200,7 +200,6 @@ export default function LandingPage() {
                 Password
               </Label>
               <div className="relative">
-                <Lock className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
                 <PasswordInput
                   id="password"
                   placeholder="Input Password"
